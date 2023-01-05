4778bd46ac728200a65dd8a08406f65387968853
Isaac Herrera <isaacheye@unisabana.edu.co>
2023-01-05 15:32:08 -0500

    update index added more info to teams scraping

--- scraping/index.js
+++ scraping/index.js
@@ -1,6 +1,8 @@
 import * as cheerio from "cheerio";
-import {writeFile} from "node:fs/promises";
+import { assert } from "node:console";
+import { writeFile } from "node:fs/promises";
 import path from "node:path";
+import TEAMS from "../db/teams.json" assert { type: "json" };
 
 const URLS = {
   leaderBoard: "https://kingsleague.pro/",
@@ -22,27 +24,41 @@ async function getLeaderBoard() {
     cardsYellow: { type: "number", selector: ".fs-table-text_8" },
     cardsRed: { type: "number", selector: ".fs-table-text_9" },
   };
+  const getTeamFromName = ({ name }) =>
+    TEAMS.find((team) => team.name === name);
+
   const LEADERBOARD_ENTRIES = Object.entries(LEADERBOARD_SELECTORS);
 
   const $rows = $("table tbody tr");
-  const result = []
+  const result = [];
   $rows.each((index, element) => {
     const $el = $(element);
 
-    const leaderBoardEntries = LEADERBOARD_ENTRIES.map(([key, {type,selector}]) => {
-      const rawValue = $el.find(selector).text().trim();
-      const number = type === "number" ? Number(rawValue) : rawValue
-      return [key, number];
-    });   
-    result.push(Object.fromEntries(leaderBoardEntries));
+    const leaderBoardEntries = LEADERBOARD_ENTRIES.map(
+      ([key, { type, selector }]) => {
+        const rawValue = $el.find(selector).text().trim();
+        const number = type === "number" ? Number(rawValue) : rawValue;
+        return [key, number];
+      }
+    );
+    const { team: teamName, ...leaderBoardForTeam } =
+      Object.fromEntries(leaderBoardEntries);
+    const team = getTeamFromName({ name: teamName });
+    
+    // insert team only if scraping was successful
+    if (team !== undefined) {
+      result.push({
+        ...leaderBoardForTeam,
+        team,
+      });
+    }
   });
-  return result;
 
+  return result;
 }
 
-
 const leaderBoard = await getLeaderBoard();
-console.log(leaderBoard)
+console.log(leaderBoard);
 
-const filePath = path.join(process.cwd(),"db","leaderboard.json")
-await writeFile(filePath,JSON.stringify(leaderBoard,null,2),'utf-8')
+const filePath = path.join(process.cwd(), "db", "leaderboard.json");
+await writeFile(filePath, JSON.stringify(leaderBoard, null, 2), "utf-8");
